4d4da3b4b6457a548e74cd082a3d008bc84dd644
Phaete <pel4nk@gmail.com>
2024-09-10 14:19:58 +0200

    Initial commit - Spring initializr

---
+++ src/main/java/com/phaete/nf45springservice/Nf45SpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phaete.nf45springservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nf45SpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nf45SpringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phaete/nf45springservice/Nf45SpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phaete.nf45springservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nf45SpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
